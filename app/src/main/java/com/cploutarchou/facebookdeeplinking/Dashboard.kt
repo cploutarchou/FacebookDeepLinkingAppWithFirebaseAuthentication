f913d1863c7be75738410093dc780a5720afca2d
Christos Plourtarchou <cploutarchou@gmail.com>
2020-08-17 22:02:32 +0300

    set some data on Dashboard.kt

--- app/src/main/java/com/cploutarchou/facebookdeeplinking/Dashboard.kt
+++ app/src/main/java/com/cploutarchou/facebookdeeplinking/Dashboard.kt
@@ -2,10 +2,36 @@ package com.cploutarchou.facebookdeeplinking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class Dashboard : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        auth = FirebaseAuth.getInstance()
+        setUserData()
+    }
+
+    private fun setUserData() {
+        val user = auth.currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+
+            setContentView(R.layout.activity_dashboard)
+
+            val textView: TextView = findViewById(R.id.logged_user_name)
+            textView.text = email.toString()
+
+            val welcome: String = getString(R.string.tv_welcome)
+            tv_welcome.text = welcome
+        }
+
     }
 }
+
