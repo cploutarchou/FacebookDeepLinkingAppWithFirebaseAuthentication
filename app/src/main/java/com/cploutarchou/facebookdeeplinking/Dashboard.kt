fa4a2aa141ef32ea8d90a85e1df88f42f0e66f9a
Christos Plourtarchou <cploutarchou@gmail.com>
2020-08-17 23:30:28 +0300

    add Change user password on firebase functionality

--- app/src/main/java/com/cploutarchou/facebookdeeplinking/Dashboard.kt
+++ app/src/main/java/com/cploutarchou/facebookdeeplinking/Dashboard.kt
@@ -1,8 +1,10 @@
 package com.cploutarchou.facebookdeeplinking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
@@ -14,6 +16,9 @@ class Dashboard : AppCompatActivity() {
         setContentView(R.layout.activity_dashboard)
         auth = FirebaseAuth.getInstance()
         setUserData()
+        btn_change_password_submit.setOnClickListener {
+            changePassword()
+        }
     }
 
     private fun setUserData() {
@@ -30,6 +35,63 @@ class Dashboard : AppCompatActivity() {
             tv_welcome.text = welcome
         }
 
+    }
+
+    private fun changePassword() {
+        if (et_original_password.text.isNotEmpty() && et_new_password.text.isNotEmpty() && et_new_password_confirm.text.isNotEmpty()) {
+            if (et_new_password.text.toString() == et_new_password_confirm.text.toString()) {
+                val user = auth.currentUser
+                if (user != null && user.email != null) {
+                    val credential = EmailAuthProvider.getCredential(
+                        user.email.toString(),
+                        et_original_password.text.toString()
+                    )
+
+                    user.reauthenticate(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Your account successfully reauthorized",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            user.updatePassword(et_new_password_confirm.text.toString())
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(
+                                            this,
+                                            "Your Password successfully updated.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        auth.signOut()
+                                        finish()
+                                    }
+                                }
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Unable to Reauthorize your account.Please Try again",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+
+                }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Password combinations not matching",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        } else {
+            Toast.makeText(
+                this,
+                "Please fill all required fields",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
     }
 }
 
