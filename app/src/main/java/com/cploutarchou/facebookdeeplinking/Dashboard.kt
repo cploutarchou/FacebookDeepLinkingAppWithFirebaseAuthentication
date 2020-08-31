0f45b59f357a21df5ab68b6772d111c00e95ce2a
Christos Plourtarchou <cploutarchou@gmail.com>
2020-08-31 22:59:38 +0300

    apply some corrections

--- app/src/main/java/com/cploutarchou/facebookdeeplinking/Dashboard.kt
+++ app/src/main/java/com/cploutarchou/facebookdeeplinking/Dashboard.kt
@@ -4,6 +4,7 @@ package com.cploutarchou.facebookdeeplinking
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -17,6 +18,7 @@ import com.facebook.login.LoginResult
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import java.util.*
 
 
 class Dashboard : AppCompatActivity() {
@@ -40,42 +42,42 @@ class Dashboard : AppCompatActivity() {
             logout()
         }
 
-        val loginFacebookBtn = findViewById<Button>(R.id.btn_facebook_login)
-        loginFacebookBtn.setOnClickListener {
+
+        var btnLoginFacebook = findViewById<Button>(R.id.btn_facebook_login)
+
+        btnLoginFacebook.setOnClickListener(View.OnClickListener {
+            // Login
             callbackManager = CallbackManager.Factory.create()
             LoginManager.getInstance()
-                .logInWithReadPermissions(this, listOf("public_profile", "email"))
+                .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
             LoginManager.getInstance().registerCallback(callbackManager,
                 object : FacebookCallback<LoginResult> {
-                    override fun onSuccess(result: LoginResult?) {
-                        if (result != null) {
-                            Log.d("Main Activity", "Facebook token:" + result.accessToken.token)
-                            startActivity(
-                                Intent(
-                                    applicationContext,
-                                    AutenticatedActivity::class.java
-                                )
-                            )
-                        }
+                    override fun onSuccess(loginResult: LoginResult) {
+                        Log.d("MainActivity", "Facebook token: " + loginResult.accessToken.token)
+                        startActivity(Intent(applicationContext, AuthenticatedActivity::class.java))
                     }
 
                     override fun onCancel() {
-                        Log.d("Main Activity", "Facebook onCancel")
+                        Log.d("MainActivity", "Facebook onCancel.")
+
                     }
 
-                    override fun onError(error: FacebookException?) {
-                        Log.d("Main Activity", "Facebook onError")
-                    }
+                    override fun onError(error: FacebookException) {
+                        Log.d("MainActivity", "Facebook onError.")
 
+                    }
                 })
-        }
+        })
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         callbackManager?.onActivityResult(requestCode, resultCode, data)
+
     }
+
 
     private fun setUserData() {
         val user = auth.currentUser
