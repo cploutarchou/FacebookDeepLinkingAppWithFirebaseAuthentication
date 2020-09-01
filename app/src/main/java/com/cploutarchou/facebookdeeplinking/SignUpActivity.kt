950ae5d77d190b54dbf286957a298220c14a9fba
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-01 22:00:05 +0300

    corrections

--- app/src/main/java/com/cploutarchou/facebookdeeplinking/SignUpActivity.kt
+++ app/src/main/java/com/cploutarchou/facebookdeeplinking/SignUpActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
+
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
